5e53c4cba5db2dd824459853081cbfdc85d794fe
Wilhelm Botha <Wilhelm@gendac.co.za>
2015-12-02 09:45:00 +0200

    Android Maps

---
+++ Android_Gmaps/Android_Gmaps/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+
+namespace Android_Gmaps
+{
+    [Activity(Label = "Android_Gmaps", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+        private GoogleMap mMap;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            SetUpMap();
+
+        }
+
+        private void SetUpMap()
+        {
+            if(mMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+            }
+            
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mMap = googleMap;
+        }
+    }
+}
+

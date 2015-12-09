1a72a0ee347dc73ae07db18a3a5267d25fc93fef
Wilhelm Botha <Wilhelm@gendac.co.za>
2015-12-09 14:58:16 +0200

    Android camera update

--- MappingApp/MappingApp/MappingApp.Droid/MainActivity.cs
+++ MappingApp/MappingApp/MappingApp.Droid/MainActivity.cs
@@ -6,6 +6,13 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using XLabs.Ioc;
+using XLabs.Platform.Services.Geolocation;
+using XLabs.Platform.Device;
+using Android.Locations;
+using Android.Content;
+using Android.Util;
+using XLabs.Platform.Services.Media;
 
 namespace MappingApp.Droid
 {
@@ -17,6 +24,12 @@ namespace MappingApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
+
+            var container = ViewModel.ViewModelLocator.Init();
+            container.Register<IGeolocator, Geolocator>();
+            container.Register<IMediaPicker, MediaPicker>();
+
             LoadApplication(new App());
         }
     }

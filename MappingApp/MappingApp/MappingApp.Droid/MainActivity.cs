e2ac5b2e77da3baef14e2ab67305c98d6cfc89a4
botha.wilhelm@gmail.com <botha.wilhelm@gmail.com>
2015-12-10 14:32:32 +0200

    Code scanner added to droid

--- MappingApp/MappingApp/MappingApp.Droid/MainActivity.cs
+++ MappingApp/MappingApp/MappingApp.Droid/MainActivity.cs
@@ -6,6 +6,21 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using XLabs.Ioc;
+using XLabs.Platform.Services.Geolocation;
+using XLabs.Platform.Device;
+using Android.Locations;
+using Android.Content;
+using Android.Util;
+using XLabs.Platform.Services.Media;
+using XLabs.Forms;
+using XLabs.Forms;
+using XLabs.Ioc;
+using XLabs.Platform.Device;
+using XLabs.Platform.Mvvm;
+using XLabs.Platform.Services;
+using XLabs.Platform.Services.Geolocation;
+using XLabs.Platform.Services.Media;
 
 namespace MappingApp.Droid
 {
@@ -17,7 +32,30 @@ namespace MappingApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
+
+            /*var container = ViewModel.ViewModelLocator.Init();
+            container.Register<IGeolocator, Geolocator>();
+            container.Register<IMediaPicker, MediaPicker>();
+            */
+
+            SetIoC();
             LoadApplication(new App());
+        }
+
+        private void SetIoC()
+        {
+            var resolverContainer = new SimpleContainer();
+
+            var app = new XFormsAppDroid();
+
+            resolverContainer.Register(t => AndroidDevice.CurrentDevice)
+                .Register<IDependencyContainer>(t => resolverContainer)
+                .Register<IXFormsApp>(app)
+                .Register<IGeolocator, Geolocator>()
+                .Register<IMediaPicker, MediaPicker>();
+
+            Resolver.SetResolver(resolverContainer.GetResolver());
         }
     }
 }
